64e9e9fd1ec937909619ed8110c17da3771dba90
hirotoppi <ueda@ir.utsunomiya-u.ac.jp>
2022-09-27 21:23:47 +0900

    tmp

---
+++ scripts/train.py
@@ -0,0 +1,20 @@
+import argparse
+import itertools
+from logging import root
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+import GPy
+from Localization.sensormodel.gp import GP
+
+from processdata import ProcessedData
+
+def get_args():
+    
+    return 0
+
+class GPTraining():
+    def __init__(self, map_id) -> None:
+        #self.map_path = 
+        pass
+
